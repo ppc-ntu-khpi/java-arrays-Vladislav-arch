b0e71f6226a53f565dcf1f7a76e1c3a353c54a31
Vladislav-arch <114737469+Vladislav-arch@users.noreply.github.com>
2023-02-14 13:19:22 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,24 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    private int[] arr = new int[2];
+    public void Calculate(){
+        while(arr[0] != 24)
+        {
+            arr[1]++;
+            if (arr[1] == 60) 
+            {
+                arr[1] = 0;
+                arr[0] += 1;
+            }
+            if (arr[0] % 10 == arr[1] / 10 && arr[0] / 10 == arr[1] % 10 ) {
+                if (arr[0] < 10)
+                    System.out.println("0" +arr[0]+":"+arr[1]);
+                else if(arr[1] < 10)
+                    System.out.println("" +arr[0]+":0"+arr[1]);
+                else
+                    System.out.println("" +arr[0]+":"+arr[1]);
+            }
+        }
     }
 }
